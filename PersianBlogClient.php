a047235eedb87029219fea78b9bafda89e4dfa50
altostratous <altostratous@gmail.com>
2016-12-12 01:36:56 +0330

    Added documentation.

--- PersianBlogClient.php
+++ PersianBlogClient.php
@@ -1,6 +1,4 @@
 <?php
-require_once('WebClient.php');
-
 /**
  * Created by PhpStorm.
  * User: HP PC
@@ -9,31 +7,47 @@ require_once('WebClient.php');
  */
 class PersianBlogClient extends WebClient
 {
-    private $url;
-
-//Upload a blank cookie.txt to the same directory as this file with a CHMOD/Permission to 777
     /**
      * PersianBlogClient constructor.
+     * @param $username The user name for persian blog account
+     * @param $password The password for persian blog account
      */
     public function __construct($username, $password)
     {
+        // first encode the values
         $username = urlencode($username);
         $password = urlencode($password);
+        // request login, the login data is captured from firefox while requesting using HttpFox firefox add-on.
         $this->login("http://persianblog.ir/Login.aspx?ReturnUrl=%2fHome.aspx", "__VIEWSTATE=%2FwEPDwULLTIwMjg2NzY4MDhkGAEFHl9fQ29udHJvbHNSZXF1aXJlUG9zdEJhY2tLZXlfXxYBBQ1DaGtSZW1lbWJlck1lULx8c80UqaSnSstbTvc%2BW8EyA%2B24CqblyFcpdU1s4Zk%3D&__VIEWSTATEGENERATOR=C2EE9ABB&TxtUsername=$username&TxtPassword=$password&btnLogin=%D9%88%D8%B1%D9%88%D8%AF");
-
     }
 
+    /**
+     * @param $blogID The blog ID, you can find it in the url while posting to a blog
+     * @param $title The title for the post
+     * @param $body The body
+     */
     public function post($blogID, $title, $body){
+        // first encode values
         $title = urlencode($title);
         $body = urlencode($body);
+        // try to load posting page
         $ch = $this->grab_page("http://persianblog.ir/CreatePost.aspx?blogID=$blogID");
-
+        // get the redirected url containing the 'h' parameter
         $last_url = curl_getinfo($ch, CURLINFO_REDIRECT_URL);
+        // post the data to the blog, the login data is captured from firefox while requesting using HttpFox firefox add-on.
         $this->post_data($last_url,
             "__EVENTTARGET=&__EVENTARGUMENT=&__VIEWSTATE=%2FwEPDwULLTExNzAxNjk2MTUPFgIeB3Bvc3RTZWMFAjIzFgICAQ9kFgICCw9kFgJmD2QWCgIBDxYCHgRUZXh0BQlwaHBwb3N0ZXJkAgMPEA8WAh4LXyFEYXRhQm91bmRnZGQWAGQCBQ8PFgIeC05hdmlnYXRlVXJsBR9odHRwOi8vcGhwcG9zdGVyLnBlcnNpYW5ibG9nLmlyZGQCBw8PFgIfAwUJSG9tZS5hc3B4ZGQCCQ9kFiJmDw8WAh8DBR1DcmVhdGVQb3N0LmFzcHg%2FYmxvZ0lEPTk0NjY1NmRkAgEPDxYCHwMFHk1hbmFnZVBvc3RzLmFzcHg%2FYmxvZ0lEPTk0NjY1NmRkAgIPDxYCHwMFHUNyZWF0ZVBhZ2UuYXNweD9ibG9nSUQ9OTQ2NjU2ZGQCAw8PFgIfAwUeTWFuYWdlUGFnZXMuYXNweD9ibG9nSUQ9OTQ2NjU2ZGQCBA8PFgIfAwUhTWFuYWdlQ29tbWVudHMuYXNweD9ibG9nSUQ9OTQ2NjU2ZGQCBQ8PFgIfAwUZQmFja3VwLmFzcHg%2FYmxvZ0lEPTk0NjY1NmRkAgYPDxYCHwMFHUNoYW5nZVRlbXAuYXNweD9ibG9nSUQ9OTQ2NjU2ZGQCBw8PFgIfAwUgRWRpdFRlbXBTdHlsZS5hc3B4P2Jsb2dJRD05NDY2NTZkZAIIDw8WAh8DBRtFZGl0VGVtcC5hc3B4P2Jsb2dJRD05NDY2NTZkZAIJDw8WAh8DBR1DdXN0b21Db2RlLmFzcHg%2FYmxvZ0lEPTk0NjY1NmRkAgoPDxYCHwMFG0VkaXRCbG9nLmFzcHg%2FYmxvZ0lEPTk0NjY1NmRkAgsPDxYCHwMFHk1hbmFnZUxpbmtzLmFzcHg%2FYmxvZ0lEPTk0NjY1NmRkAgwPDxYCHwMFIE1hbmFnZUF1dGhvcnMuYXNweD9ibG9nSUQ9OTQ2NjU2ZGQCDQ8PFgIfAwUbRWRpdFRhZ3MuYXNweD9ibG9nSUQ9OTQ2NjU2ZGQCDg8PFgIfAwUfRWRpdEJsb2cuYXNweD9ibG9nSUQ9OTQ2NjU2I2Z0cGRkAg8PDxYCHwMFM2h0dHA6Ly93d3cucGVyc2lhbnN0YXQuY29tL1Jlc3VsdHMuYXNweD9pZD00MDk0NjY1NmRkAhAPFgIfAQUkPGEgaHJlZj0iUFMuYXNweCI%2B2KrZhti424zZhdin2Ko8L2E%2BZBgBBR5fX0NvbnRyb2xzUmVxdWlyZVBvc3RCYWNrS2V5X18WCQUJY2hrRXh0cmExBQRjWWVzBQVjQXBwcgUFY0FwcHIFA2NObwUDY05vBQdwUHVibGljBQpwUHJvdGVjdGVkBQpwUHJvdGVjdGVkuZZCbbUSbXIQP%2FEDSoFe8uLczaubgRpPyHfZkOTKJ4Q%3D&__VIEWSTATEGENERATOR=79457F1B&blogID=946656&blogName=phpposter&securityToken=9e4ad10ee5c9e9b46bba25996ba144c923904d13&TxtTitle=$title&intrContent=$body&keyWord1=&keyWord2=&keyWord3=&keyWord4=&mainContent=&postDay=22&postMonth=09&postYear=1395&postMinute=03&postHour=10&Commenting=cYes&PostPreview=pPublic&txtPostPasswd=&btnPublish=%D8%A7%D9%86%D8%AA%D8%B4%D8%A7%D8%B1"
             );
     }
 
+    /**
+     * Logs in to the site and saves cookie.txt
+     * Find full documentation here
+     * https://www.youtube.com/watch?v=_kQN-3aNCeI
+     * @param $url the url to login to
+     * @param $data the data to send
+     * @return mixed returns curl result
+     */
     function login($url, $data){
         $fp = fopen("cookie.txt", "w");
         fclose($fp);
@@ -54,6 +68,13 @@ class PersianBlogClient extends WebClient
         unset($login);
     }
 
+    /**
+     * Grabs a website page
+     * Find full documentation here
+     * https://www.youtube.com/watch?v=_kQN-3aNCeI
+     * @param $site The site to grab
+     * @return resource The curl handle
+     */
     function grab_page($site){
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
@@ -62,12 +83,22 @@ class PersianBlogClient extends WebClient
         curl_setopt($ch, CURLOPT_COOKIEFILE, "cookie.txt");
         curl_setopt($ch, CURLOPT_URL, $site);
         ob_start();
+        // changed here to return the curl handle
         curl_exec ($ch);
         return $ch;
         ob_end_clean();
         curl_close ($ch);
     }
 
+
+    /**
+     * Posts data to url
+     * Find full documentation here:
+     * https://www.youtube.com/watch?v=_kQN-3aNCeI
+     * @param $site the url
+     * @param $data the data url encoded
+     * @return mixed returns curl_exec result
+     */
     function post_data($site,$data){
         $datapost = curl_init();
         $headers = array("Expect:");
--- index.php
+++ index.php
@@ -1,17 +1,30 @@
 <?php
 
+// include the Class
 require_once('PersianBlogClient.php');
+
+// if both title and body files exist
 if (file_exists("title.txt") && file_exists("body.txt")) {
+    // flag to perform posting
     $doPost = true;
+    // get content of body file
     $body = file_get_contents("body.txt");
+    // get content of title file
     $title = file_get_contents("title.txt");
+    // if post hash file exists
     if (file_exists("hash")) {
+        // compare hash, if it is the same
         if (md5($title.$body) == file_get_contents("hash"))
+            // we won't post
             $doPost = false;
     }
+    // if the post flag is set
     if ($doPost) {
+        // create client
         $webClient = new PersianBlogClient("phpposter", "phpposter");
+        // post to the blog
         $webClient->post('946656', $title, $body);
+        // put the new post hash for further check
         file_put_contents("hash", md5($title.$body));
     }
 }
