abfcdcfbe3111773d78ff48a746dbcbe5394a12a
alto <altostratous@gmail.com>
2017-01-12 17:40:13 +0330

    Success in blogfa - alhamdo le Allah.

--- BlogClient.php
+++ BlogClient.php
@@ -6,11 +6,12 @@ require_once ('vendor/autoload.php');
  * Date: 1/12/17
  * Time: 3:19 PM
  */
-class BlogClient
+abstract class BlogClient
 {
 
     protected $blog_tag;
     protected $browser;
+
     /**
      * BlogClient constructor.
      * @param $username
@@ -18,6 +19,7 @@ class BlogClient
      * @param $password
      * @param $password_field
      * @param $submit_selector
+     * @param $login_url
      * @param $blog_tag
      */
     public function __construct(
@@ -26,12 +28,27 @@ class BlogClient
         $password,
         $password_field,
         $submit_selector,
+        $login_url,
         $blog_tag
         )
     {
+        // set field
+        $this->blog_tag = $blog_tag;
         // setup browser
         $this->browser = Facebook\WebDriver\Remote\RemoteWebDriver::create(
             'http://localhost:4444/wd/hub',
             \Facebook\WebDriver\Remote\DesiredCapabilities::firefox());
+        $this->browser->get($login_url);
+        $this->browser->findElement(\Facebook\WebDriver\WebDriverBy::name($username_field))->sendKeys($username);
+        $this->browser->findElement(\Facebook\WebDriver\WebDriverBy::name($password_field))->sendKeys($password);
+        $this->browser->findElement(\Facebook\WebDriver\WebDriverBy::cssSelector($submit_selector))->click();
+    }
+
+    public abstract function send_post($title, $content, $blog_tag = null);
+    public abstract function submit_post();
+    public function general_send_post($title, $content, $title_field, $content_field){
+        $this->browser->findElement(\Facebook\WebDriver\WebDriverBy::name($title_field))->sendKeys($title);
+        $this->browser->findElement(\Facebook\WebDriver\WebDriverBy::name($content_field))->sendKeys($content);
+        $this->submit_post();
     }
 }
--- BlogfaBlogClient.php
+++ BlogfaBlogClient.php
@@ -10,7 +10,32 @@ class BlogfaBlogClient extends BlogClient
 {
     public function __construct($username, $password, $blog_tag = null)
     {
-        parent::__construct($username, 'usrid', $password, 'usrpass', 'input[name=btnSubmit]', $blog_tag);
+        parent::__construct(
+            $username,
+            'usrid',
+            $password,
+            'usrpass',
+            'input[name=btnSubmit]',
+            'http://blogfa.com/',
+            $blog_tag);
     }
 
+    public function send_post($title, $content, $blog_tag = null)
+    {
+        $this->browser->wait()->until(function ($driver){
+            return strpos($driver->getCurrentUrl(), 'https://blogfa.com/Desktop/Default.aspx') !== false;
+        });
+        $this->browser->get('https://blogfa.com/Desktop/home.aspx?type=first');
+        $this->browser->findElement(\Facebook\WebDriver\WebDriverBy::tagName('a'))->click();
+        $this->browser->wait()->until(function ($driver){
+            return count($driver->findElements(\Facebook\WebDriver\WebDriverBy::id('btnPublish'))) > 0;
+        });
+        $this->general_send_post($title, $content, 'txtTitle', 'txtPostBody');
+    }
+
+    public function submit_post()
+    {
+        sleep(5);
+        $this->browser->findElement(\Facebook\WebDriver\WebDriverBy::cssSelector('#btnPublish'))->click();
+    }
 }
